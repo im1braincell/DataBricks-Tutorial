ad7bf9124464720ea0942d6eac347e2fb09e344d
im1braincell <65070213@kmitl.ac.th>
2026-01-23 20:58:26 +0700

    Update and rename testquery.dbquery.ipynb to testquery.sql

---
+++ testquery.sql
@@ -0,0 +1,12 @@
+-- update
+select Name, stageName from opportunity
+where Name = 'DataFlux Expansion 543' 
+
+-- del
+select * from contact
+where firstname = 'Tina'
+and lastname = 'Zimmermann'
+and phone = '(842) 576-6344'
+
+-- insert
+-- account inserted check
